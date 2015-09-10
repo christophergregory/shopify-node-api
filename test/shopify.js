76294c37b429f5778d1726e36cfdf5f368451b95
Chua Kiem Fai <kievechua@gmail.com>
2015-09-11 06:25:57 +0800

    Allow is_valid_signature to handle extra query

--- test/shopify.js
+++ test/shopify.js
@@ -80,11 +80,12 @@ describe('#is_valid_signature', function(){
                 shopify_shared_secret: 'hush'
             }),
             params = {
+                'protocol': 'https://',
                 'shop': 'some-shop.myshopify.com',
                 'code': 'a94a110d86d2452eb3e2af4cfb8a3828',
                 'timestamp': '1337178173',
                 'signature': '6e39a2ea9e497af6cb806720da1f1bf3',
-                'hmac': '2cb1a277650a659f1b11e92a4a64275b128e037f2c3390e3c8fd2d8721dac9e2'
+                'hmac': '5bbba3ca1967bb481598fd46b37e004d9608d04f0a4b7b60352d7995cd7d1d28'
             };
 
         expect(Shopify.is_valid_signature(params)).to.equal(true);
